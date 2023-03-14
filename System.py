57a8f546ca4556c351eb0029fdebbb190fb82209
kittibest46@gmail.com0 <kittibest46@gmail.com>
2023-03-14 16:29:26 +0700

    <feat>:community

---
+++ System.py
@@ -0,0 +1,25 @@
+from Community import *
+
+class System:
+    def __init__(self) -> None:
+        self.boards = []
+        self.product_catalog = []
+        self.user_account = {} # id:password
+
+    def ceate_user(self):
+        pass
+
+    def confirm_payment(self):
+        pass
+
+    def verify_login(self):
+        pass
+
+    def register(self,id,password1,password2):
+        pass
+
+    def verify_password(self,password1,password2):
+        pass
+
+    def generate_id(self):
+        pass
