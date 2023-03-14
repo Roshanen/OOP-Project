5bef253204c4cc126bbd53ea10401d125b64828f
Kachen Thongsawatwong <65010100@kmitl.ac.th>
2023-03-14 16:25:21 +0700

    User.setup

---
+++ Class/User.py
@@ -0,0 +1,16 @@
+class User:
+    def __init__(self, user_id, password, name, profile_picture, description, level, recent_activities, following, comments, game_playing):
+        self.__user_id = user_id
+        self.__password = password
+        self.__name = name
+        self.__profile_picture = profile_picture
+        self.__description = description
+        self.__level = level
+        self.__recent_activities = recent_activities
+        self.__following = following
+        self.__comments = comments
+        self._game_playing = game_playing
+    def method():
+        pass
+    def method2():
+        pass
