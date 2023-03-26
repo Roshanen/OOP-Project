5e3d0b7b54baaf1b350ba7d1a3c43f0db7f6e51c
Kachen <106764256+Roshanen@users.noreply.github.com>
2023-03-26 23:24:29 +0700

    <feat>: update user attributes & methods

--- User.py
+++ User.py
@@ -1,29 +1,37 @@
+from Order import *
+from ShoppingCart import *
+from Library import *
+from System import *
+import datetime
+
 class User:
-    def __init__(self, user_id, password, name, profile_picture, description, level):
-        self.__user_id = user_id
-        self.__password = password
+    def __init__(self, name, id, password, profile_picture=None, description=None, level=0) :
         self.__name = name
+        self.__id = id
+        self.__password = password
         self.__profile_picture = profile_picture
         self.__description = description
         self.__level = level
+        self.__order = Order(self.__id)
+        self.__cart = ShoppingCart(self.__id)
+        self.__library = Library()
+        mySystem.system_add_user(self)
 
-    def make_payment():
-        pass
+    def get_name(self):
+        return self.__name
+    
+    def get_id(self):
+        return self.__id
 
-    def search_profile(user_info):
-        pass
+    def get_cart(self):
+        return self.__cart
+
+    def view_cart(self):
+        return self.__cart.view_products()
     
-    def search_product(keyword):
-        pass
-    
-    def view_profile(user_info):
-        pass
-    
-    def view_product(product_info):
-        pass
-    
-    def view_cart():
-        pass
-    
-    def login(user_id, password):
-        pass
+    def add_products(self, product):
+        self.__cart.add_products(product)
+        return self.__cart
+
+    def get_library(self):
+        return self.__library
