e672946bb7f7ea378771abaa48ad882f4b0d840a
Krittanaputch Choopinij <paulkrit47@gmail.com>
2023-03-28 17:49:04 +0700

    <feat> update date_time

--- UserItem.py
+++ UserItem.py
@@ -1,55 +1,49 @@
 from datetime import datetime
 
 class Item:
-  def __init__(self, name = '', picture = '', got_date = '',description = '', status = '', amount = 0):
-    self.__name = name
-    self.__picture = picture
-    self.__got_date = got_date
-    self.__description = description
-    self.__status = status
-    self.__amount = amount
+  def __init__(self, name='', picture='', description='', status='', amount=0, got_date=None):
+    self.name = name
+    self.picture = picture
+    self.description = description
+    self.status = status
+    self.amount = amount
+    if got_date is None:
+      self.got_date = datetime.now()
+    else:
+      self.got_date = got_date
 
 class Badge:
-  def __init__(self, name = '', picture = '', got_date = '',description = '', status = ''):
-    self.__name = name
-    self.__picture = picture
-    self.__got_date = got_date
-    self.__description = description
-    self.__status = status
+  def __init__(self, name='', picture='', description='', status='', got_date=None):
+    self.name = name
+    self.picture = picture
+    self.description = description
+    self.status = status
+    if got_date is None:
+      self.got_date = datetime.now()
+    else:
+      self.got_date = got_date
 
 class Factory:
   def __init__(self, product, user):
-    self.__product = product
-    self.__user = user
-    self.__item_creator = ItemCreator()
-    self.__badge_creator = BadgeCreator()
+    self.product = product
+    self.user = user
+    self.item_creator = ItemCreator()
+    self.badge_creator = BadgeCreator()
 
-  def send_item(self, name, picture, got_date, description, status, amount):
-    item = self.create_item(name, picture, got_date, description, status, amount)
-    self.__user.add_item(item)
+  def send_item(self, name, picture, description, status, amount, got_date=None):
+    item = self.item_creator.create_item(name, picture, description, status, amount, got_date)
+    self.user.add_item(item)
 
-  def create_item(self, name, picture, got_date, description, status, amount):
-    item = {'name ': name, 'picture': picture, 'got_date': got_date, 'description': description, 'status': status, 'amount': amount}
+  def send_badge(self, name, picture, description, status, got_date=None):
+    badge = self.badge_creator.create_badge(name, picture, description, status, got_date)
+    self.user.add_badge(badge)
+
+class ItemCreator:
+  def create_item(self, name, picture, description, status, amount, got_date=None):
+    item = Item(name, picture, description, status, amount, got_date)
     return item
-  
-  def create_badge(self, name, picture, got_date, description, status):
-    badge = {'name ': name, 'picture': picture, 'got_date': got_date, 'description': description, 'status': status}
+
+class BadgeCreator:
+  def create_badge(self, name, picture, description, status, got_date=None):
+    badge = Badge(name, picture, description, status, got_date)
     return badge
-
-class ItemCreator(Factory):
-  def __init__(self):
-    pass
-
-  def create_item(self, name, picture, got_date, description, status, amount):
-    super().create_item(name, picture, got_date, description, status, amount)
-
-class BadgeCreator(Factory):
-  def __init__(self):
-    pass
-
-  def create_badge(self, name, picture, got_date, description, status):
-    super().create_badge(name, picture, got_date, description, status)
-
-
-date = datetime.now()
-print(date.strftime("%Y-%m-%d %H:%M:%S"))
