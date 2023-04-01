bb4c1336e7a2b0a2b7e112a5fc4610c766f79b2a
best <kittibest46@gmail.com>
2023-04-02 00:19:08 +0700

    <refactor>:remove u_id of comment

--- Community.py
+++ Community.py
@@ -1,4 +1,5 @@
 import time
+from System import *
 
 class Post:
     def __init__(self,u_id,poster,media) -> None:
@@ -26,6 +27,9 @@ class Rating:
         self.__good_rating = 0
         self.__bad_rating = 0
 
+    def get_rating(self):
+        return self.__bad_rating,self.__good_rating
+
     def add_rating(self):
         self.__good_rating += 1
     
@@ -33,15 +37,20 @@ class Rating:
         self.__bad_rating += 1
 
 class Comment:
-    def __init__(self,u_id,comment) -> None:
-        self.__u_id = u_id
+    def __init__(self,comment) -> None:
         self.__comment = comment
         self.__date = time.localtime
         self.__replies = []
     
-    def reply(self,u_id,reply):
-        reply = Comment(u_id,reply)
+    def reply(self,reply):
+        reply = Comment(reply)
         self.__replies.append(reply)
+
+    def get_comment(self):
+        return self.__comment
+
+    def get_replies(self):
+        return self.__replies
 
 class Board:
     def __init__(self,post:list) -> None:
@@ -50,3 +59,5 @@ class Board:
     def add_post(self,new_post):
         self.__post.append(new_post)
 
+    def get_post(self):
+        return self.__post
