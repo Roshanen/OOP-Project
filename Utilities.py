0a0e82475f818d6bfb44f72ec0f869e9f1961223
best <kittibest46@gmail.com>
2023-04-04 23:23:59 +0700

    <feat>: change variable name and control the type of it

--- Utilities.py
+++ Utilities.py
@@ -3,5 +3,5 @@ import hashlib
 class IdGenerator:
     @staticmethod
     def generate_id(to_hash):
-        id = hashlib.md5(to_hash.encode()).hexdigest()
+        id = hashlib.md5(str(to_hash).encode()).hexdigest()
         return id
