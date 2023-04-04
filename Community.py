3508fe307e09330eb03687e5b0880c9c122c5733
best <kittibest46@gmail.com>
2023-04-04 23:25:29 +0700

    <feat>: add id to get_info method

--- Community.py
+++ Community.py
@@ -1,8 +1,10 @@
-import time
-from WebSystem import *
+import datetime
+import json
+from Utilities import IdGenerator
 
 class Post:
     def __init__(self,poster,media):
+        self.__id = IdGenerator.generate_id(id(self))
         self.__poster = poster
         self.__media_link = media
         self.__rating = Rating()
@@ -10,6 +12,7 @@ class Post:
 
     def get_info(self):
         info = {
+            "id" : self.__id,
             "poster" : self.__poster,
             "media" : self.__media_link,
             "rating" : self.__rating,
@@ -20,8 +23,8 @@ class Post:
 
 class Rating:
     def __init__(self) -> None:
-        self.__thumbs_up_link = ""
-        self.__thumbs_down_link = ""
+        self.__thumbs_up_pic = ""
+        self.__thumbs_down_pic = ""
         self.__good_rating = 0
         self.__bad_rating = 0
 
@@ -37,7 +40,7 @@ class Rating:
 class Comment:
     def __init__(self,comment) -> None:
         self.__comment = comment
-        self.__date = time.localtime
+        self.__date = datetime.datetime.now()
         self.__replies = []
     
     def reply(self,reply):
@@ -51,8 +54,8 @@ class Comment:
         return self.__replies
 
 class Board:
-    def __init__(self,post:list) -> None:
-        self.__post = post
+    def __init__(self) -> None:
+        self.__post = []
 
     def add_post(self,new_post):
         self.__post.append(new_post)
