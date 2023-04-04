3bd0813d37ee9641038bad111166b78fde75829b
best <kittibest46@gmail.com>
2023-04-04 22:46:15 +0700

    <feat>:update testing file to be able to test RestAPI

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import datetime
 from WebSystem import *
 from Community import *
 from User import *
+from typing import Optional
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
+app = FastAPI()
 
 steen_system = System()
 
-steen_system.register(user_name = "Best",email = "dark97975@gmail.com",password1 = "12345Best",password2 = "12345Best")
+steen_system.register(user_name="Paul",email="dark97975@gmail.com",password1="123Paul!",password2="123Paul!")
 
 steen_system.add_product({
             "name": "Mario",
@@ -39,4 +43,44 @@ steen_system.add_product({
             "release_date": datetime.datetime.now()
             })
 
-print(steen_system.search_product("Mario"))
+@app.get("/",response_class=HTMLResponse)
+async def show_default():
+    return \
+    """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Title</title>
+    </head>
+    <body>
+        Hello world
+    </body>
+    </html>
+    """
+
+@app.get("/search_profile")
+async def search_profile(name = None,id = None):
+    found_user = steen_system.search_profile(search_name=name,search_id=id)
+    return found_user
+
+@app.get("/search_product")
+async def search_product(name = None):
+    found_products = steen_system.search_product(search_name=name)
+    return found_products
+
+@app.post("/register")
+async def register(username,email,pass1,pass2):
+    status = steen_system.register(user_name=username,email=email,password1=pass1,password2=pass2)
+    return status
+
+@app.post("/login")
+async def register(email,password):
+    status = steen_system.login(email=email,password=password)
+    return status
+
+@app.post("/view_user_profile")
+async def register(email,password):
+    status = steen_system.login(email=email,password=password)
+    return status
+
