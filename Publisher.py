c63f68b128feb5191cca7d0a63d550d94e91c3df
Kachen <106764256+Roshanen@users.noreply.github.com>
2023-04-18 01:29:50 +0700

    <feat>: move method to system

--- Publisher.py
+++ Publisher.py
@@ -2,12 +2,3 @@ class Publisher:
  def __init__(self,prfile_picture,links_to_other_site):
   self.__profile_picture = profile_picture
   self.__links_to_other_site = links_to_other_site
- 
-def create_product(product_info):
-  pass
-
-def modify(new_info):
-  pass
-
-def delete_product(product_info):
-  pass
