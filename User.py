b601dbbb10e892c1a85948b34ce5e9c92b8a27b2
Kachen <106764256+Roshanen@users.noreply.github.com>
2023-04-18 02:15:45 +0700

    <feat>: update user's method

--- User.py
+++ User.py
@@ -2,36 +2,67 @@ from Order import *
 from ShoppingCart import *
 from Library import *
 from System import *
+from Chat import *
+from Factory import *
+
 import datetime
 
+
 class User:
-    def __init__(self, name, id, password, profile_picture=None, description=None, level=0) :
+    def __init__(self, system, name, profile_picture=None, description=None, level=0):
         self.__name = name
-        self.__id = id
-        self.__password = password
+        self.__id = system.generate_id(name)
         self.__profile_picture = profile_picture
         self.__description = description
         self.__level = level
         self.__order = Order(self.__id)
         self.__cart = ShoppingCart(self.__id)
         self.__library = Library()
-        mySystem.system_add_user(self)
+        self.__chats = {}
+        self.__badge = []
+        system.add_user(self)
 
     def get_name(self):
         return self.__name
-    
+
     def get_id(self):
         return self.__id
+    
+    @property
+    def profile_picture(self):
+        return self.__profile_picture
+    
+    @profile_picture.setter
+    def picture_profile(self,picture):
+        self.__profile_picture = picture
+        
+    @property
+    def description(self):
+        return self.__description
+    
+    @property
+    def level(self, level):
+        self.__level = level
 
     def get_cart(self):
         return self.__cart
 
     def view_cart(self):
         return self.__cart.view_products()
-    
+
     def add_products(self, product):
         self.__cart.add_products(product)
         return self.__cart
 
     def get_library(self):
         return self.__library
+
+    def add_chat(self, chat):
+        self.__chats[chat.get_id()] = chat
+
+    def request_item(self, factory):
+        factory.check_condition(self)
+        
+    def add_badge(self, badge):
+        if badge != None:
+            self.__badge.append(badge)
