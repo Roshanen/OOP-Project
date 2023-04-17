bc4bf52e4e6879842f2a807b3963399c96127814
best <kittibest46@gmail.com>
2023-04-18 02:47:04 +0700

    <feat>: mount css and add more route to more page

--- main.py
+++ main.py
@@ -17,37 +17,6 @@ steen_system = System()
 
 steen_system.register(user_name="Paul",email="dark97975@gmail.com",password1="123Paul!",password2="123Paul!")
 
-steen_system.add_product({
-            "name": "Mario",
-            "price": 219,
-            "os_support": "WINDOW-MACOS-LINUX",
-            "system_req": "2.4GHz-NVIDIA1050",
-            "tags": "KUECHIARAI",
-            "cover_image": None,
-            "lang_sup": "Thai-Bangladesh-English-Japanese",
-            "ban_country": None,
-            "exc_country": "Thailand",
-            "age_rate": "20+",
-            "discount": 0,
-            "description": "This is not suit you",
-            "release_date": datetime.datetime.now()
-            })
-steen_system.add_product({
-            "name": "Dog",
-            "price": 399,
-            "os_support": "WINDOW-MACOS",
-            "system_req": "3.2GHz-NVIDIA3050",
-            "tags": "KUECHIARAI",
-            "cover_image": None,
-            "lang_sup": "English-Japanese",
-            "ban_country": None,
-            "exc_country": None,
-            "age_rate": "5+",
-            "discount": 0,
-            "description": "Play this till the end of da day",
-            "release_date": datetime.datetime.now()
-            })
-
 template = Jinja2Templates("Template")
 
 @app.get("/",response_class=HTMLResponse)
