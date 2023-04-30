61e0b1f50e0c6d3518a23eb216fcc377741da050
Kachen Thongsawatwong <65010100@kmitl.ac.th>
2023-04-30 18:16:55 +0700

    Delete all files

--- order.py
+++
@@ -1,9 +0,0 @@
-class Order:
-    def __init__(self, id, total_price=0, date=None):
-        self.__id = id
-        self.__products = []
-        self.__total_price = total_price
-        self.__date = date
-
-    def choose_method(system):
-        return system.verify_payment()
